b5744010b66bf314d54b96e343451afacb083531
SageScroll18144 <lipe.fssantos063@gmail.com>
2019-12-16 14:49:38 -0300

    melhorando

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ earth = vp.sphere(pos=vector(0,0,0), radius=1, texture = "http://i.imgur.com/rhF
 sun = vp.sphere(pos=vector(0,0,0), radius=4, texture =  "http://i.imgur.com/yoEzbtg.jpg")
 t = 0
 
-scene.autoscale = False
-theta=2*3.14159/365 
+scene.autoscale = True
+theta=2*3.14159/24 
 EarthToSun = 50
 while True:
     rate(10)
     earth.pos.x = EarthToSun*cos(t)
     earth.pos.z = EarthToSun*sin(t)
-    earth.rotate(angle=theta*0.2, axis=vector(0,1,0), origin=vector(0,0,0))
+    earth.rotate(angle=theta, axis=vector(0,1,0), origin=vector(0,0,0))
     sun.rotate(angle=theta*0.2, axis=vector(0,1,0), origin=vector(0,0,0))
     t+=0.01
