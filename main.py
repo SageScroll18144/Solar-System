d01350da0590705e963bdf09250d3a065fb977c9
SageScroll18144 <lipe.fssantos063@gmail.com>
2019-12-16 11:19:19 -0300

    Ta melhor assim

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import vpython as vp
 from vpython import *
 
-earth = vp.sphere(pos=vector(50,0,0), radius=1, texture = "http://i.imgur.com/rhFu01b.jpg", make_trail=True)
+earth = vp.sphere(pos=vector(0,0,0), radius=1, texture = "http://i.imgur.com/rhFu01b.jpg", make_trail=True)
 sun = vp.sphere(pos=vector(0,0,0), radius=4, texture =  "http://i.imgur.com/yoEzbtg.jpg")
 t = 0
 
 scene.autoscale = False
-theta=2*3.14159/365 #vel ang
-
+theta=2*3.14159/365 
+EarthToSun = 50
 while True:
-    rate(60)
-
-    earth.rotate(angle=theta, axis=vector(0,1,0), origin=vector(0,0,0))
-     
+    rate(10)
+    earth.pos.x = EarthToSun*cos(t)
+    earth.pos.z = EarthToSun*sin(t)
+    earth.rotate(angle=theta*0.2, axis=vector(0,1,0), origin=vector(0,0,0))
+    sun.rotate(angle=theta*0.2, axis=vector(0,1,0), origin=vector(0,0,0))
     t+=0.01
-    
-
-
